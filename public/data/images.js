7c6648f85b732f875f55bf0a3a743e4cf8e94128
Graham Lewis <gclikkec@mac.com>
2014-10-08 13:27:57 +0100

    change images based on month

---
+++ public/data/images.js
@@ -0,0 +1,58 @@
+var index_images = [ 
+  [
+    "/public/images/index/image-set-1/image-0.jpg",
+    "/public/images/index/image-set-1/image-1.jpg",
+    "/public/images/index/image-set-1/image-2.jpg",
+    "/public/images/index/image-set-1/image-3.jpg",
+    "/public/images/index/image-set-1/image-4.jpg",
+    "/public/images/index/image-set-1/image-5.jpg",
+    "/public/images/index/image-set-1/image-6.jpg",
+    "/public/images/index/image-set-1/image-7.jpg",
+    "/public/images/index/image-set-1/image-8.jpg",
+    "/public/images/index/image-set-1/image-9.jpg"
+  ],[
+    "/public/images/index/image-set-2/image-0.jpg",
+    "/public/images/index/image-set-2/image-1.jpg",
+    "/public/images/index/image-set-2/image-2.jpg",
+    "/public/images/index/image-set-2/image-3.jpg",
+    "/public/images/index/image-set-2/image-4.jpg",
+    "/public/images/index/image-set-2/image-5.jpg",
+    "/public/images/index/image-set-2/image-6.jpg",
+    "/public/images/index/image-set-2/image-7.jpg",
+    "/public/images/index/image-set-2/image-8.jpg",
+    "/public/images/index/image-set-2/image-9.jpg"
+  ],[
+    "/public/images/index/image-set-3/image-0.jpg",
+    "/public/images/index/image-set-3/image-1.jpg",
+    "/public/images/index/image-set-3/image-2.jpg",
+    "/public/images/index/image-set-3/image-3.jpg",
+    "/public/images/index/image-set-3/image-4.jpg",
+    "/public/images/index/image-set-3/image-5.jpg",
+    "/public/images/index/image-set-3/image-6.jpg",
+    "/public/images/index/image-set-3/image-7.jpg",
+    "/public/images/index/image-set-3/image-8.jpg",
+    "/public/images/index/image-set-3/image-9.jpg"
+  ]
+];
+
+var what_we_do_index_images = [ 
+  [
+    [
+      "/public/images/what-we-do/index/image-set-1/image-0.jpg",
+      "/public/images/what-we-do/index/image-set-2/image-0.jpg",
+      "/public/images/what-we-do/index/image-set-3/image-0.jpg"
+    ],[
+      "/public/images/what-we-do/index/image-set-1/image-1.jpg",
+      "/public/images/what-we-do/index/image-set-2/image-1.jpg",
+      "/public/images/what-we-do/index/image-set-3/image-1.jpg"
+    ],[
+      "/public/images/what-we-do/index/image-set-1/image-2.jpg",
+      "/public/images/what-we-do/index/image-set-2/image-2.jpg",
+      "/public/images/what-we-do/index/image-set-3/image-2.jpg"
+    ],[
+      "/public/images/what-we-do/index/image-set-1/image-3.jpg",
+      "/public/images/what-we-do/index/image-set-2/image-3.jpg",
+      "/public/images/what-we-do/index/image-set-3/image-3.jpg"
+    ]
+  ]
+];
---
+++ public/js/app.js
@@ -0,0 +1,83 @@
+function getOffset() {
+  var date = new Date();
+  // var month = date.getMonth();
+  var month = date.getHours();
+  var offset = month % 3;
+  return offset;
+}
+
+
+function createSlider(imageSet, id) {
+  var slider = "<div id='" + id + "' class='carousel slide'>";
+  
+  slider += "<ol class='carousel-indicators'>";
+  for (var i = 0; i < imageSet.length; i++) {
+    if (i === 0) {
+      slider += "<li data-target='#" + id + "' data-slide-to='" + i + "' class='active'></li>";
+    } else {
+      slider += "<li data-target='#" + id + "' data-slide-to='" + i + "'></li>";
+    }
+  }
+  slider += "</ol>";
+  
+  slider += "<div class='carousel-inner'>";
+  for (var i = 0; i < imageSet.length; i++) {
+    if (i === 0) {
+      slider += "<div class='item active'>";      
+    } else {
+      slider += "<div class='item'>";      
+    }
+  
+    slider += "<img src='" + imageSet[i] + "'>";
+    slider += "</div>";
+  }
+    
+  slider += "</div>";
+  
+  slider += "<a class='left carousel-control' href='#" + id + "' data-slide='prev'>";
+  slider += "<i class='icon-angle-left'></i>";
+  slider += "</a>";
+
+  slider += "<a class='right carousel-control' href='#" + id + "' data-slide='next'>";
+  slider += "<i class='icon-angle-right'></i>";
+  slider += "</a>";
+
+  slider += "</div>";
+  slider += "<div class='image-over-wording-slider'></div>";
+  
+  return slider;
+}
+
+
+function createSwapperImages(imageSet) {
+  var swapperImages = "";
+  
+  for (var i = 0; i < imageSet.length; i++) {
+    if (i === 0) {
+      swapperImages += "<img src='" + imageSet[i] + "' class='active img-responsive img-rounded'>";
+    } else {
+      swapperImages += "<img src='" + imageSet[i] + "' class='img-responsive img-rounded'>";
+    }
+  }
+  
+  return swapperImages;
+}
+
+
+function swapImages() {
+  $(".swapper-image .active").each(function() {
+    var next = null;
+    if ($(this).next().length > 0) {
+      next = $(this).next();
+    } else {
+      next = $(this).parent().children().first();
+    }
+
+    $(this).fadeOut(1000, function() {
+      next.fadeIn(1000).addClass('active');
+      $(this).removeClass('active');
+    });
+  });
+}
+
+
