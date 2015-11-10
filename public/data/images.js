168d4d93bf6ff010ed3ffb8bf47371eef3b0d169
rosalind-redengine <rosalind.steven@redengine.tv>
2015-11-10 10:02:26 +0000

    auto commit by Red Engine

--- public/data/images.js
+++ public/data/images.js
@@ -95,11 +95,11 @@ var what_we_do_index_images = [
 
 var print_images = [ 
   [
+    "/public/images/what-we-do/print-new/print_wedding_venues.jpg",
+    "/public/images/what-we-do/print-new/print_ambra_todd.jpg",
     "/public/images/what-we-do/print-new/print_brochure_comp.jpg",
     "/public/images/what-we-do/print-new/print_card_brochure.jpg",
     "/public/images/what-we-do/print-new/print_parkins_fabrics.jpg",
-    "/public/images/what-we-do/print-new/print_select_travel.jpg",
-    "/public/images/what-we-do/print-new/print_wedding_venues.jpg",
-    "/public/images/what-we-do/print-new/print_ambra_todd.jpg"
+    "/public/images/what-we-do/print-new/print_select_travel.jpg"
   ]
 ];
