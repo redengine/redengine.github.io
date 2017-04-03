ce177271341eb7d1b590fb451b9881a27c1e948f
Sue Jarman <sue@suejarman.co.uk>
2017-04-03 21:31:32 +0000

    Auto commit

--- gitPush.sh
+++ gitPush.sh
@@ -1,3 +1,3 @@
 git add --all
 git commit -am 'Auto commit' --all
-git push https://rosalind-redengine@github.com/redengine.github.io
+git push https://rosalind-redengine@github.com/redengine/redengine.github.io
